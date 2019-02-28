6c7c0a2a7f5eccafd06a2d176a0b6b7d3729255e
Samir Agarwala <samir.aggarwala@gmail.com>
2019-02-28 12:09:56 -0800

    Run file with instructions for BW conversion

--- run.py
+++ run.py
@@ -9,3 +9,6 @@ processing.cram2bam()
 
 # Convert BAM to BedGraph using bedtools: genomeCoverageBed -ibam seq.bam -bg -trackline -split -g ... > out.bedGraph
 # Convert BedGraph to BigWig: bedGraphToBigWig out.bedGraph chrom.sizes out.bw
+	#In chrom.sizes file of assembly Hg19: remove chr before chr1,chrX, etc. Also change chromMt to MT.
+	# Note: Displaying particular line in data file: sed -n 'eline_num,40p' file_name
+	# Note: Delete particular line in data file using eg. for line 3: sed -i '3d' filename. In our case last line was being problematic and thus deleted.
